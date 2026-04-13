1932223fbf5d6054aa99ed23de0c03f59a19dd2c
Sthefany Pereira dos passos <sthefanyp337@gmail.com>
2026-04-13 16:39:32 -0300

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace testando_showhide1
+﻿namespace crud
 {
-    partial class Form1
+    partial class frmCadastrodeClientes
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,55 +28,247 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.btmMostraslogo = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelCadastrodecliente = new System.Windows.Forms.Panel();
+            this.lblCadastrodecliente = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.maskedTextBox4 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblNomeSocial = new System.Windows.Forms.Label();
+            this.lblNomeCompleto = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.LstCliente = new System.Windows.Forms.ListView();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panelCadastrodecliente.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // panelCadastrodecliente
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(235, 44);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(386, 214);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.panelCadastrodecliente.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panelCadastrodecliente.Controls.Add(this.lblCadastrodecliente);
+            this.panelCadastrodecliente.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelCadastrodecliente.Location = new System.Drawing.Point(0, 0);
+            this.panelCadastrodecliente.Name = "panelCadastrodecliente";
+            this.panelCadastrodecliente.Size = new System.Drawing.Size(865, 99);
+            this.panelCadastrodecliente.TabIndex = 0;
             // 
-            // btmMostraslogo
+            // lblCadastrodecliente
             // 
-            this.btmMostraslogo.BackColor = System.Drawing.Color.MediumBlue;
-            this.btmMostraslogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btmMostraslogo.ForeColor = System.Drawing.Color.LightCoral;
-            this.btmMostraslogo.Location = new System.Drawing.Point(371, 286);
-            this.btmMostraslogo.Name = "btmMostraslogo";
-            this.btmMostraslogo.Size = new System.Drawing.Size(120, 44);
-            this.btmMostraslogo.TabIndex = 1;
-            this.btmMostraslogo.Text = "Mostras Logo";
-            this.btmMostraslogo.UseVisualStyleBackColor = false;
-            this.btmMostraslogo.Click += new System.EventHandler(this.btmMostraslogo_Click);
+            this.lblCadastrodecliente.AutoSize = true;
+            this.lblCadastrodecliente.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCadastrodecliente.ForeColor = System.Drawing.Color.White;
+            this.lblCadastrodecliente.Location = new System.Drawing.Point(47, 35);
+            this.lblCadastrodecliente.Name = "lblCadastrodecliente";
+            this.lblCadastrodecliente.Size = new System.Drawing.Size(215, 25);
+            this.lblCadastrodecliente.TabIndex = 0;
+            this.lblCadastrodecliente.Text = "Cadastro de Cliente";
             // 
-            // Form1
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(23, 105);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(792, 289);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.maskedTextBox4);
+            this.tabPage1.Controls.Add(this.maskedTextBox3);
+            this.tabPage1.Controls.Add(this.maskedTextBox2);
+            this.tabPage1.Controls.Add(this.maskedTextBox1);
+            this.tabPage1.Controls.Add(this.lblCPF);
+            this.tabPage1.Controls.Add(this.lblEmail);
+            this.tabPage1.Controls.Add(this.lblNomeSocial);
+            this.tabPage1.Controls.Add(this.lblNomeCompleto);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(784, 263);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Dados de Cliente";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // maskedTextBox4
+            // 
+            this.maskedTextBox4.Location = new System.Drawing.Point(137, 186);
+            this.maskedTextBox4.Mask = "###,###,###-##";
+            this.maskedTextBox4.Name = "maskedTextBox4";
+            this.maskedTextBox4.Size = new System.Drawing.Size(256, 20);
+            this.maskedTextBox4.TabIndex = 7;
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.Location = new System.Drawing.Point(137, 132);
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.Size = new System.Drawing.Size(281, 20);
+            this.maskedTextBox3.TabIndex = 6;
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(189, 75);
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(281, 20);
+            this.maskedTextBox2.TabIndex = 5;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(189, 29);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(281, 20);
+            this.maskedTextBox1.TabIndex = 1;
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPF.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.lblCPF.Location = new System.Drawing.Point(31, 184);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(40, 20);
+            this.lblCPF.TabIndex = 4;
+            this.lblCPF.Text = "CPF";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.lblEmail.Location = new System.Drawing.Point(31, 132);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(53, 20);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "E-mail";
+            // 
+            // lblNomeSocial
+            // 
+            this.lblNomeSocial.AutoSize = true;
+            this.lblNomeSocial.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeSocial.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.lblNomeSocial.Location = new System.Drawing.Point(31, 73);
+            this.lblNomeSocial.Name = "lblNomeSocial";
+            this.lblNomeSocial.Size = new System.Drawing.Size(98, 20);
+            this.lblNomeSocial.TabIndex = 2;
+            this.lblNomeSocial.Text = "Nome Social";
+            // 
+            // lblNomeCompleto
+            // 
+            this.lblNomeCompleto.AutoSize = true;
+            this.lblNomeCompleto.BackColor = System.Drawing.SystemColors.Window;
+            this.lblNomeCompleto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeCompleto.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.lblNomeCompleto.Location = new System.Drawing.Point(31, 27);
+            this.lblNomeCompleto.Name = "lblNomeCompleto";
+            this.lblNomeCompleto.Size = new System.Drawing.Size(123, 20);
+            this.lblNomeCompleto.TabIndex = 1;
+            this.lblNomeCompleto.Text = "Nome Completo";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.LstCliente);
+            this.tabPage2.Controls.Add(this.btnPesquisar);
+            this.tabPage2.Controls.Add(this.txtBuscar);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(784, 263);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Consulta";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // LstCliente
+            // 
+            this.LstCliente.HideSelection = false;
+            this.LstCliente.Location = new System.Drawing.Point(27, 62);
+            this.LstCliente.Name = "LstCliente";
+            this.LstCliente.Size = new System.Drawing.Size(730, 186);
+            this.LstCliente.TabIndex = 2;
+            this.LstCliente.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.BackColor = System.Drawing.Color.DarkMagenta;
+            this.btnPesquisar.ForeColor = System.Drawing.Color.White;
+            this.btnPesquisar.Location = new System.Drawing.Point(302, 22);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(101, 29);
+            this.btnPesquisar.TabIndex = 1;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = false;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(25, 27);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(188, 20);
+            this.txtBuscar.TabIndex = 0;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.SkyBlue;
+            this.btnSalvar.ForeColor = System.Drawing.Color.White;
+            this.btnSalvar.Location = new System.Drawing.Point(350, 402);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(176, 36);
+            this.btnSalvar.TabIndex = 2;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click_1);
+            // 
+            // frmCadastrodeClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btmMostraslogo);
-            this.Controls.Add(this.pictureBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Click += new System.EventHandler(this.Form1_Click);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ClientSize = new System.Drawing.Size(865, 450);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panelCadastrodecliente);
+            this.Name = "frmCadastrodeClientes";
+            this.Text = "Cadastro";
+            this.panelCadastrodecliente.ResumeLayout(false);
+            this.panelCadastrodecliente.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button btmMostraslogo;
+        private System.Windows.Forms.Panel panelCadastrodecliente;
+        private System.Windows.Forms.Label lblCadastrodecliente;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblNomeSocial;
+        private System.Windows.Forms.Label lblNomeCompleto;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox4;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.ListView LstCliente;
+        private System.Windows.Forms.ColorDialog colorDialog1;
     }
 }
 
